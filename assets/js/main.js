bac9ad61e33cb77a926675e09e28d3ed004b3e5e
temptado <jason1287712@gmail.com>
2022-08-10 20:44:54 -0400

    added script to calculate bmi through calculate form

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,5 +51,49 @@ window.addEventListener('scroll', scrollHeader);
 
 /*=============== CALCULATE JS ===============*/
 
+const calculateForm = document.getElementById('calculate-form'),
+    calculateCm = document.getElementById('calculate-cm'),
+    calculateKg = document.getElementById('calculate-kg'),
+    calculateMessage = document.getElementById('calculate-message');
+
+const calculateBmi = (e) => {
+    e.preventDefault();
+
+    if (calculateCm.value === '' || calculateKg.value === '') {
+        calculateMessage.classList.remove('color-green');
+        calculateMessage.classList.add('color-red');
+
+        calculateMessage.textContent = 'Fill in the Height and Weight';
+
+        setTimeout(() => {
+            calculateMessage.textContent = '';
+        }, 3000);
+    } else {
+        const cm = calculateCm.value / 100,
+            kg = calculateKg.value,
+            bmi = Math.round(kg / (cm * cm));
+
+        if (bmi < 18.5) {
+            calculateMessage.classList.add('color-green');
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are skinny`;
+        } else if (bmi < 25) {
+            calculateMessage.classList.add('color-green');
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are healthy`;
+        } else {
+            calculateMessage.classList.add('color-green');
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are overweight`;
+        }
+
+        calculateCm.value = '';
+        calculateKg.value = '';
+
+        setTimeout(() => {
+            calculateMessage.textContent = ''
+        }, 4000);
+    }
+
+}
+
+calculateForm.addEventListener('submit', calculateBmi);
 
 /*=============== EMAIL JS ===============*/
