3798ea5cc014ac88e8b201126fe770b143782649
temptado <jason1287712@gmail.com>
2022-08-15 19:43:19 -0400

    added script to show scroll up buttong

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,6 +65,13 @@ window.addEventListener('scroll', scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/
 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+};
+
+window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
